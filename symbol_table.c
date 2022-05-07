cb86da03b07cf319361bb38f330b53cd5dbf0340
tzuyichan <F04066028@gs.ncku.edu.tw>
2022-05-07 16:48:50 +0800

    Initial commit

---
+++ symbol_table.c
@@ -0,0 +1,6 @@
+#include "symbol_table.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ symbol_table.h
@@ -0,0 +1,11 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int add(int, int);
+
+#endif
